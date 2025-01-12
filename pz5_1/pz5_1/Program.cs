d4c4431736d3f730f4f430fdd72f49622ce2c3c8
Avgust3 <popov.avgustin@gmail.com>
2025-01-12 17:13:32 +0900

    Add files via upload

---
+++ pz5_1/pz5_1/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("введите число");
+int number = Int32.Parse(Console.ReadLine()); //Int32.Parse преобразует строковое представление числа в его 32-битный подписанный целочисленный эквивалент
+Console.WriteLine($"вот какое число вы ввели {number}");
